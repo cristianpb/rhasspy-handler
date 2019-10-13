3298e1b4851e8bee8a71a59c1ab83ae18eb5ad00
cristianpb <felipebrokate@gmail.com>
2019-10-13 18:26:30 +0200

    Save radios in radio.js file

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 const mqtt = require('mqtt');
-require('dotenv').config()
 const Mopidy = require("mopidy");
 const Parser = require('rss-parser');
 const fs = require('fs');
 const Path = require('path');
 const axios = require('axios');
 const CronJob = require('cron').CronJob;
+require('dotenv').config()
 const hostname = process.env.HOST ;
 const client  = mqtt.connect(`mqtt://${hostname}`);
-const RADIOS = {
-  'vibra': ['tunein:station:s84760'],
-  'la mega': ['tunein:station:s86588'],
-  'la joya': ['tunein:station:s192004'],
-  'la cariñosa': ['tunein:station:s143839'],
-  'caracol': ['tunein:station:s16182'],
-  'clásica': ['tunein:station:s25732'],
-  'suavecita': ['tunein:station:s33937'],
-  'ascensor': ['http://somafm.com/groovesalad.pls'],
-  'covers': ['http://somafm.com/covers.pls'],
-  'indie': ['http://somafm.com/indiepop.pls'],
-  'lounge': ['http://somafm.com/illstreet.pls'],
-  'regetón': ['tunein:station:s269960'],
-  'vallenato': ['tunein:station:s297590'],
-  'las noticias': ['podcast+http://radiofrance-podcast.net/podcast09/rss_12494.xml'],
-  'noventa': ['tunein:station:s89818']
-}
+const RADIOS = require('./radios');
 
 new CronJob({
 	// At minute 0 past every hour from 9 through 21.”
---
+++ radios.js
@@ -0,0 +1,19 @@
+const RADIOS = {
+  'vibra': ['tunein:station:s84760'],
+  'la mega': ['tunein:station:s86588'],
+  'la joya': ['tunein:station:s192004'],
+  'la cariñosa': ['tunein:station:s143839'],
+  'caracol': ['tunein:station:s16182'],
+  'clásica': ['tunein:station:s25732'],
+  'suavecita': ['tunein:station:s33937'],
+  'ascensor': ['http://somafm.com/groovesalad.pls'],
+  'covers': ['http://somafm.com/covers.pls'],
+  'indie': ['http://somafm.com/indiepop.pls'],
+  'lounge': ['http://somafm.com/illstreet.pls'],
+  'regetón': ['tunein:station:s269960'],
+  'vallenato': ['tunein:station:s297590'],
+  'las noticias': ['podcast+http://radiofrance-podcast.net/podcast09/rss_12494.xml'],
+  'noventa': ['tunein:station:s89818']
+}
+
+module.export = RADIOS;
