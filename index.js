3dd99bf727eff7195ee3c4fa6bd2025adce21361
cristianpb <felipebrokate@gmail.com>
2019-11-11 23:23:10 +0100

    Linter modifications

--- index.js
+++ index.js
@@ -6,40 +6,44 @@ const axios = require('axios');
 const shell = require('shelljs');
 const CronJob = require('cron').CronJob;
 require('dotenv').config()
-const hostname = process.env.HOST ;
-const client  = mqtt.connect(`mqtt://${hostname}`);
+const hostname = process.env.HOST;
+const client = mqtt.connect(`mqtt://${hostname}`);
 const SnipsMopidy = require('./snipsmopidy');
 
-new CronJob({
-  // At minute 0 past every hour from 9 through 21.”
+let cron = new CronJob({
+  // At minute 0 past every hour from 9 through 21.
   cronTime: '00 9-21 * * *',
-  onTick: function () {
+  onTick: () => {
     let currentTime = new Date();
-    client.publish("hermes/tts/say", `{"siteId":"default","lang": "es", "text":"Son las ${currentTime.toTimeString().substring(0, 2)}"}`)
+    client.publish(
+      'hermes/tts/say',
+      `{"siteId":"default","lang": "es", "text":"Son las ${currentTime
+        .toTimeString()
+        .substring(0, 2)}"}`
+    );
   },
   start: true,
   timeZone: 'Europe/Paris'
 });
-
 
 if (process.env.BLUETOOTH == 'true') {
   const blue = require('blue');
 }
 
 /* On Connect MQTT */
-client.on("connect", function() {
-  console.log("[Snips Log] Connected to MQTT broker " + hostname);
+client.on('connect', () => {
+  console.log('[Snips Log] Connected to MQTT broker ' + hostname);
   downloadPostcast(
-    "http://fapi-top.prisasd.com/podcast/caracol/la_luciernaga/itunestfp/podcast.xml"
+    'http://fapi-top.prisasd.com/podcast/caracol/la_luciernaga/itunestfp/podcast.xml'
   );
   client.subscribe('hermes/#');
 });
 
 /* On Message */
 client.on('message', function (topic, message) {
-  if (topic === "hermes/asr/startListening") {
+  if (topic === 'hermes/asr/startListening') {
     onListeningStateChanged(true);
-  } else if (topic === "hermes/asr/stopListening") {
+  } else if (topic === 'hermes/asr/stopListening') {
     onListeningStateChanged(false);
   } else if (topic.match(/hermes\/hotword\/.+\/detected/g) !== null) {
     onHotwordDetected()
@@ -49,27 +53,27 @@ client.on('message', function (topic, message) {
 });
 
 /* Snips actions */
-function onIntentDetected(intent) {
+function onIntentDetected (intent) {
   console.log(`[Snips Log] Intent detected: ${JSON.stringify(intent)}`);
   const {intent: {intentName} = null} = intent;
-  if (intentName == 'cristianpb:radioOn') {
+  if (intentName === 'cristianpb:radioOn') {
     SnipsMopidy.radioOn(intent);
-  } else if (intentName == 'cristianpb:speakerInterrupt') {
+  } else if (intentName === 'cristianpb:speakerInterrupt') {
     SnipsMopidy.stopMopidy();
-  } else if (intentName == 'cristianpb:playArtist') {
+  } else if (intentName === 'cristianpb:playArtist') {
     console.log(intent);
     const {slots = null} = intent
     if ((slots) && (slots.length > 0)) {
       const {value = null} = slots[0];
       SnipsMopidy.searchArtist([value.value]);
     }
-  } else if (intentName == 'cristianpb:volumeDown') {
+  } else if (intentName === 'cristianpb:volumeDown') {
     SnipsMopidy.volumeDown();
-  } else if (intentName == 'cristianpb:nextSong') {
+  } else if (intentName === 'cristianpb:nextSong') {
     SnipsMopidy.nextSong();
-  } else if (intentName == 'cristianpb:volumeUp') {
+  } else if (intentName === 'cristianpb:volumeUp') {
     SnipsMopidy.volumeUp();
-  } else if (intentName == 'cristianpb:volumeSet') {
+  } else if (intentName === 'cristianpb:volumeSet') {
     const {slots = null} = intent
     if ((slots) && (slots.length > 0)) {
       const {value = null} = slots[0]
@@ -77,48 +81,48 @@ function onIntentDetected(intent) {
     } else {
       SnipsMopidy.speak(`No entendí ${intentName}`);
     }
-  } else if (intentName == 'cristianpb:restartApplication') {
+  } else if (intentName === 'cristianpb:restartApplication') {
     const {slots = null} = intent
     if ((slots) && (slots.length > 0)) {
       const {value = null} = slots[0]
-      console.log("GET", value['value']);
-      if (value['value'] == 'Snips') restartCommand(`systemctl restart "snips-*"`);
-      if (value['value'] == 'Mopidy') restartCommand('systemctl restart mopidy.service', 'Mopidy reiniciado');
-      if (value['value'] == 'Aplicación') restartCommand('systemctl restart handler.service', 'Applicacion reininicada');
-      if (value['value'] == 'Raspberry') restartCommand('reboot', 'Reiniciado');
+      console.log('GET', value['value']);
+      if (value['value'] === 'Snips') restartCommand(`systemctl restart "snips-*"`);
+      if (value['value'] === 'Mopidy') restartCommand('systemctl restart mopidy.service', 'Mopidy reiniciado');
+      if (value['value'] === 'Aplicación') restartCommand('systemctl restart handler.service', 'Applicacion reininicada');
+      if (value['value'] === 'Raspberry') restartCommand('reboot', 'Reiniciado');
     } else {
       SnipsMopidy.speak(`No entendí ${intentName}`);
     }
   } else {
-    SnipsMopidy.speak("No se que hacer");
+    SnipsMopidy.speak('No se que hacer');
   }
 }
 
-function onHotwordDetected() {
+function onHotwordDetected () {
   client.publish(
     'hermes/tts/say',
     `{"siteId":"default","lang": "es", "text":"Si?"}`
   );
-  console.log("[Snips Log] Hotword detected");
+  console.log('[Snips Log] Hotword detected');
 }
-function onListeningStateChanged(listening) {
-  console.log("[Snips Log] " + (listening ? "Start" : "Stop") + " listening");
+function onListeningStateChanged (listening) {
+  console.log('[Snips Log] ' + (listening ? 'Start' : 'Stop') + ' listening');
 }
 
-function restartCommand(command, message) {
-  shell.exec(command, function(code, stdout, stderr) {
+function restartCommand (command, message) {
+  shell.exec(command, function (code, stdout, stderr) {
     if (stderr) {
-      SnipsMopidy.speak("Hay un pequeno problema");
+      SnipsMopidy.speak('Hay un pequeno problema');
     } else {
       SnipsMopidy.speak(message);
     }
   });
 }
 
-async function downloadPostcast(url) {
+async function downloadPostcast (url) {
   let parser = new Parser();
   let feed = await parser.parseURL(url);
-  console.log("RSS: ", feed.title);
+  console.log('RSS: ', feed.title);
 
   const dir = process.env.PODCAST_DIR; // '/var/lib/mopidy/media'
   const files = fs
@@ -126,14 +130,14 @@ async function downloadPostcast(url) {
     .map(fileName => {
       return {
         name: fileName,
-        time: fs.statSync(dir + "/" + fileName).mtime.getTime()
+        time: fs.statSync(dir + '/' + fileName).mtime.getTime()
       };
     })
     .sort((a, b) => b.time - a.time) // Sort decending order
     .map(v => v.name);
 
   feed.items.slice(0, 5).forEach(async item => {
-    let pieces = item.guid.split("/");
+    let pieces = item.guid.split('/');
     console.log(`Checking : ${pieces[pieces.length - 1]}`);
     if (files.indexOf(pieces[pieces.length - 1]) === -1) {
       console.log(`Downloading ${item.title}:${item.guid}`);
@@ -152,18 +156,18 @@ async function downloadPostcast(url) {
   }
 }
 
-async function downloadFile(guid, pieces) {
+async function downloadFile (guid, pieces) {
   let path = Path.resolve(process.env.PODCAST_DIR, pieces[pieces.length - 1]);
   const writer = fs.createWriteStream(path);
   const response = await axios({
     url: guid,
-    method: "GET",
-    responseType: "stream"
+    method: 'GET',
+    responseType: 'stream'
   });
   response.data.pipe(writer);
 
   return new Promise((resolve, reject) => {
-    writer.on("finish", resolve);
-    writer.on("error", reject);
+    writer.on('finish', resolve);
+    writer.on('error', reject);
   });
 }
--- snipsmopidy.js
+++ snipsmopidy.js
@@ -1,119 +1,113 @@
 const fs = require('fs');
 const mqtt = require('mqtt');
-const Mopidy = require("mopidy");
+const Mopidy = require('mopidy');
 const RADIOS = require('./radios');
-const hostname = process.env.HOST ;
-const mqtt_client = mqtt.connect(`mqtt://${hostname}`);
-const mopidy_client = new Mopidy({
-  webSocketUrl: `ws://${hostname}:6680/mopidy/ws/`,
+const hostname = process.env.HOST;
+const mqttClient = mqtt.connect(`mqtt://${hostname}`);
+const mopidyClient = new Mopidy({
+  webSocketUrl: `ws://${hostname}:6680/mopidy/ws/`
 });
 
 class SnipsMopidy {
-  constructor() {
-    //this.mqtt_client = mqtt.connect(`mqtt://${hostname}`);
-  }
-
-  speak(text) {
+  speak (text) {
     console.log('READING', text);
-    mqtt_client.publish("hermes/tts/say", `{"siteId":"default","lang": "es", "text": "${text}"}`)
+    mqttClient.publish('hermes/tts/say', `{"siteId":"default","lang": "es", "text": "${text}"}`)
   }
 
-  volumeSet(volumeNumber) {
+  volumeSet (volumeNumber) {
     console.log(`Volume set to ${volumeNumber}`)
-    mopidy_client.mixer.setVolume([Number(volumeNumber)]);
+    mopidyClient.mixer.setVolume([Number(volumeNumber)]);
     this.speak(`Volumen a ${volumeNumber}`);
   }
 
-  volumeUp() {
+  volumeUp () {
     console.log('Volume up');
-    mopidy_client.mixer.setVolume([50]);
+    mopidyClient.mixer.setVolume([50]);
     this.speak('Volumen alto');
   }
 
   async volumeDown () {
     console.log('Volume down');
-    mopidy_client.mixer.setVolume([5])
+    mopidyClient.mixer.setVolume([5])
     this.speak('Volumen bajo');
   }
 
-  async stopMopidy() {
+  async stopMopidy () {
     console.log('Stopping moppidy');
-    await mopidy_client.playback.stop()
-    await mopidy_client.tracklist.clear()
+    await mopidyClient.playback.stop()
+    await mopidyClient.tracklist.clear()
     this.speak('Silencio');
   }
 
-  async setRadio(radio_name, radioUri) {
-    await mopidy_client.tracklist.clear()
-    await mopidy_client.playback.pause()
-    let tracks = await mopidy_client.tracklist.add({uris: radioUri})
+  async setRadio (radioName, radioUri) {
+    await mopidyClient.tracklist.clear()
+    await mopidyClient.playback.pause()
+    let tracks = await mopidyClient.tracklist.add({uris: radioUri})
     console.log('Tracks', tracks);
     if (radioUri[0].includes('podcast')) {
       tracks = [tracks[tracks.length - 1]]
     }
     try {
-      this.speak(`Voy a sintonizar ${radio_name}`);
-      await mopidy_client.playback.play({tlid: tracks[0].tlid});
+      this.speak(`Voy a sintonizar ${radioName}`);
+      await mopidyClient.playback.play({tlid: tracks[0].tlid});
     } catch (e) {
       console.log(e);
-      await mopidy_client.playback.stop()
+      await mopidyClient.playback.stop()
       this.speak('Tengo un pequeño problema');
     }
   }
 
-  setMyRadios(radio) {
+  setMyRadios (radio) {
     let radioUri = RADIOS[radio]
     this.setRadio(radio, radioUri)
   }
 
-  async searchArtist(Value) {
+  async searchArtist (Value) {
     console.log(`Searching for ${Value}`)
     this.speak(`Buscando canciones de ${Value}`);
-    let result = await mopidy_client.library.search({'any': Value, 'uris': ['soundcloud:']})
+    let result = await mopidyClient.library.search({'any': Value, 'uris': ['soundcloud:']})
     console.log(result);
     let songUri = result[0]['tracks'].map(item => item.uri);
     console.log('Playin', songUri);
     this.setRadio(Value, songUri);
   }
 
-  nextSong() {
+  nextSong () {
     console.log('Next');
     this.speak('Siguiente canción');
-    mopidy_client.playback.next();
+    mopidyClient.playback.next();
   }
 
-  radioOn(intent) {
+  radioOn (intent) {
     console.log(intent);
     const {slots = null} = intent;
     if ((slots) && (slots.length > 0)) {
       const {value = null} = slots[0];
       if (Object.keys(RADIOS).indexOf(value.value) >= 0) {
         this.setMyRadios(value.value);
-      } else if (value.value == 'la luciérnaga') {
+      } else if (value.value === 'la luciérnaga') {
         const files = fs.readdirSync(process.env.PODCAST_DIR);
-        console.log(`file://${process.env.PODCAST_DIR}/${files[files.length-1]}`);
-        this.setRadio('la luciérnaga', [`file://${process.env.PODCAST_DIR}/${files[files.length-1]}`, `file://${process.env.PODCAST_DIR}/${files[files.length-2]}`]);
+        console.log(`file://${process.env.PODCAST_DIR}/${files[files.length - 1]}`);
+        this.setRadio('la luciérnaga', [`file://${process.env.PODCAST_DIR}/${files[files.length - 1]}`, `file://${process.env.PODCAST_DIR}/${files[files.length-2]}`]);
       } else {
-        this.speak("Radio desconocida")
-        console.log("Unkown");
+        this.speak('Radio desconocida')
+        console.log('Unkown');
       }
     } else {
-      SnipsMopidy.speak("No entendí");
+      SnipsMopidy.speak('No entendí');
     }
   }
-
-
 };
 
 const snipsmopidy = new SnipsMopidy()
-mopidy_client.on("state:online", async () => {
-	snipsmopidy.speak("Connectado");
+mopidyClient.on('state:online', async () => {
+  snipsmopidy.speak('Connectado');
   snipsmopidy.volumeSet(13);
   //snipsmopidy.setMyRadios('ascensor');
   //snipsmopidy.searchArtist('ozuna');
   //snipsmopidy.nextSong();
 })
-mopidy_client.on("state:offline", async () => {
-	snipsmopidy.speak("Desconectado");
-})
+//mopidyClient.on('state:offline', async () => {
+//  snipsmopidy.speak('Desconectado');
+//})
 module.exports = snipsmopidy;
