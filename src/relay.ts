f213c271c372d7d24395ca86f25e8a69e5c6525a
cristianpb <felipebrokate@gmail.com>
2020-04-13 08:42:57 +0200

    ✨ Add button push functionality

--- src/relay.ts
+++ src/relay.ts
@@ -1,16 +1,25 @@
-const Gpio = require('onoff').Gpio;
+import { Gpio, BinaryValue } from 'onoff';
 
-let LEDPin: any;
+let button: Gpio;
+let LEDPin: Gpio;
 
 if (process.env.DEVICE == "raspberry") {
   LEDPin = new Gpio(12, 'out'); // declare GPIO4 an output
+  //button = new Gpio(17, 'in', 'both');
+  button = new Gpio(17, 'in', 'rising', {debounceTimeout: 10});
+  button.watch((err, _) => {
+    if (err) {
+      throw err;
+    }
+    LEDPin.writeSync((LEDPin.readSync() ^ 1) as BinaryValue);
+  });
 }
 
 export function blinking (blink_time = 2000) {
 	const startValue = LEDPin.readSync()
 
 	// Toggle the state of the LED
-	const iv = setInterval(_ => LEDPin.writeSync(LEDPin.readSync() ^ 1), 500);
+	const iv = setInterval(_ => LEDPin.writeSync((LEDPin.readSync() ^ 1) as BinaryValue), 500);
 
 	// Stop blinking the LED after blink_time
 	setTimeout(_ => {
@@ -23,8 +32,13 @@ export function blinking (blink_time = 2000) {
 	}, blink_time);
 }
 
-export function changeState (arg: number) {
+export function changeState (arg: BinaryValue) {
   if (process.env.DEVICE == "raspberry") {
     LEDPin.writeSync(arg)
   }
 }
+
+process.on('SIGINT', _ => {
+  LEDPin.unexport();
+  button.unexport();
+});
